91dd5fa05bd6183bb2f17f6dae404b43ffff4d24
Emiyassstar <emiyasstar@outlook.com>
2016-06-15 18:13:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/emiyasstar/functiondemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.emiyasstar.functiondemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView) EditText username;
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
